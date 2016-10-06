f4f7ec75d64b26d94ca541fd3b60a58982e31536
Octavio <octavio.hermida.97@gmail.com>
2016-10-06 14:06:16 -0700

    Wrote code in R

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -15,3 +15,12 @@
 # Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
 
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
+
+hometown <- "Monterrey"
+my.name <- "Octavio Hermida"
+my.height <- "1.73 m"
+puppies <- 2
+puppy.price <- 100
+total.cost <- puppies * puppy.price
+too.expensive <- total.cost > 1000
+max.puppies <- 1000/puppy.price
